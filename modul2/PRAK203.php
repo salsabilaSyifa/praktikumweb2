ed2318c9202ccf84fac27f6f04bbef791683fcef
SALSABILA SYIFA <101609243+salsabilaSyifa@users.noreply.github.com>
2022-03-24 09:45:17 +0800

    Update PRAK203.php

--- modul2/PRAK203.php
+++ modul2/PRAK203.php
@@ -45,97 +45,106 @@
             $suhu=$_POST['suhu'];
             $nilai=$_POST['nilai'];
             $konversi=$_POST['konversi'];
+            
 
-            switch ($nilai=="celcius") {
-                case $konversi=="reamur":
-                    $hasil=(4/5)*$suhu;
-                    echo "<h3>";
-                    echo " Hasil konversi ke Reamur: $hasil °R";
-                    echo "</h3>";
-                break;
-                
-                case $konversi=="fahrenhait":
-                    $hasil=(9/5)*$suhu+32;
-                    echo "<h3>";
-                    echo "Hasil Konversi ke Fahrenhait : $hasil °F";
-                    echo "</h3>";
-                break;
-    
-                case $konversi=="kelvin":
-                    $hasil=$suhu+273;
-                    echo "<h3>";
-                    echo "Hasil Konversi ke Kelvin: $hasil °K";
-                    echo "</h3>";
-                break;
+            if ($_POST["nilai"]=="celcius") {
+                switch ($nilai=="celcius") {
+                    case $konversi=="reamur":
+                        $hasil=(4/5)*$suhu;
+                        echo "<h3>";
+                        echo " Hasil konversi Celcius ke Reamur: $hasil °R";
+                        echo "</h3>";
+                    break;
+                    
+                    case $konversi=="fahrenhait":
+                        $hasil=(9/5)*$suhu+32;
+                        echo "<h3>";
+                        echo "Hasil Konversi Celcius ke Fahrenhait : $hasil °F";
+                        echo "</h3>";
+                    break;
+        
+                    case $konversi=="kelvin":
+                        $hasil=$suhu+273;
+                        echo "<h3>";
+                        echo "Hasil Konversi Celcius ke Kelvin: $hasil °K";
+                        echo "</h3>";
+                    break;
+                }
             }
 
-            switch ($nilai=="reamur") {
-                case $konversi=="celcius":
-                    $hasil=(5/4)*$suhu;
-                    echo "<h3>";
-                    echo " Hasil konversi ke Celcius: $hasil °C";
-                    echo "</h3>";
-                break;
-                
-                case $konversi=="fahrenhait":
-                    $hasil=(9/4)*$suhu+32;
-                    echo "<h3>";
-                    echo "Hasil Konversi ke Fahrenhait : $hasil °F";
-                    echo "</h3>";
-                break;
-    
-                case $konversi=="kelvin":
-                    $hasil=(5/4)*$suhu+273;
-                    echo "<h3>";
-                    echo "Hasil Konversi ke Kelvin: $hasil °K";
-                    echo "</h3>";
-                break;
+            else if ($_POST["nilai"]=="reamur") {
+                switch ($nilai=="reamur") {
+                    case $konversi=="celcius":
+                        $hasil=(5/4)*$suhu;
+                        echo "<h3>";
+                        echo " Hasil konversi Reamur ke Celcius: $hasil °C";
+                        echo "</h3>";
+                    break;
+                    
+                    case $konversi=="fahrenhait":
+                        $hasil=(9/4)*$suhu+32;
+                        echo "<h3>";
+                        echo "Hasil Konversi Reamur ke Fahrenhait : $hasil °F";
+                        echo "</h3>";
+                    break;
+        
+                    case $konversi=="kelvin":
+                        $hasil=(5/4)*$suhu+273;
+                        echo "<h3>";
+                        echo "Hasil Konversi Reamur ke Kelvin: $hasil °K";
+                        echo "</h3>";
+                    break;
+                }
             }
 
-            switch ($nilai=="fahrenhait") {
-                case $konversi=="celcius":
-                    $hasil=(5/9)*($suhu-32);
-                    echo "<h3>";
-                    echo " Hasil konversi ke Celcius: $hasil °C";
-                    echo "</h3>";
-                break;
-                
-                case $konversi=="reamur":
-                    $hasil=(4/9)*($suhu-32);
-                    echo "<h3>";
-                    echo "Hasil Konversi ke Reamur : $hasil °R";
-                    echo "</h3>";
-                break;
-    
-                case $konversi=="kelvin":
-                    $hasil=(5/9)*($suhu-32)+273;
-                    echo "<h3>";
-                    echo "Hasil Konversi ke Kelvin: $hasil °K";
-                    echo "</h3>";
-                break;
+            else if ($_POST["nilai"]=="fahrenhait") {
+                switch ($nilai=="fahrenhait") {
+                    case $konversi=="celcius":
+                        $hasil=(5/9)*($suhu-32);
+                        echo "<h3>";
+                        echo " Hasil konversi Fahrenhait ke Celcius: $hasil °C";
+                        echo "</h3>";
+                    break;
+                    
+                    case $konversi=="reamur":
+                        $hasil=(4/9)*($suhu-32);
+                        echo "<h3>";
+                        echo "Hasil Konversi Fahrenhait ke Reamur : $hasil °R";
+                        echo "</h3>";
+                    break;
+        
+                    case $konversi=="kelvin":
+                        $hasil=(5/9)*($suhu-32)+273;
+                        echo "<h3>";
+                        echo "Hasil Konversi Fahrenhait ke Kelvin: $hasil °K";
+                        echo "</h3>";
+                    break;
+                }
             }
 
-            switch ($nilai=="kelvin") {
-                case $konversi=="celcius":
-                    $hasil=$suhu-273;
-                    echo "<h3>";
-                    echo " Hasil konversi ke Celcius: $hasil °C";
-                    echo "</h3>";
-                break;
-                
-                case $konversi=="reamur":
-                    $hasil=(4/5)*($suhu-273);
-                    echo "<h3>";
-                    echo "Hasil Konversi ke Reamur : $hasil °R";
-                    echo "</h3>";
-                break;
-    
-                case $konversi=="fahrenhait":
-                    $hasil=(9/5)*($suhu-273)+32;
-                    echo "<h3>";
-                    echo "Hasil Konversi ke Fahrenhait: $hasil °F";
-                    echo "</h3>";
-                break;
+            else if ($_POST["nilai"]=="kelvin") {
+                switch ($nilai=="kelvin") {
+                    case $konversi=="celcius":
+                        $hasil=$suhu-273;
+                        echo "<h3>";
+                        echo " Hasil konversi Kelvin ke Celcius: $hasil °C";
+                        echo "</h3>";
+                    break;
+                    
+                    case $konversi=="reamur":
+                        $hasil=(4/5)*($suhu-273);
+                        echo "<h3>";
+                        echo "Hasil Konversi Kelvin ke Reamur : $hasil °R";
+                        echo "</h3>";
+                    break;
+        
+                    case $konversi=="fahrenhait":
+                        $hasil=(9/5)*($suhu-273)+32;
+                        echo "<h3>";
+                        echo "Hasil Konversi Kelvin ke Fahrenhait: $hasil °F";
+                        echo "</h3>";
+                    break;
+                }
             }
         }
     }
